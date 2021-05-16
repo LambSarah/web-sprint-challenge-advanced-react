f5bce2a0c7be0a0ba99fd20c5fe7c47648158f74
Sarah Guidry <sarahbethguidry@gmail.com>
2021-05-16 00:27:00 -0500

    Added in 'filter plants in PlantList' feature

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -15,9 +15,36 @@ export const useForm = (initialValues) => {
 
     // onSubmit handler form form
     const handleSubmit = e => {
+        //prevent page from re-rendering
         e.preventDefault();
         // makes success message visible
         setState({ ...state, showSuccessMessage: true })
     }
     return [state, handleChanges, handleSubmit];
+
 }
+
+
+
+/*{this.state?.plants?.map((plant) => (
+         <div className="plant-card" key={plant.id} data-testid="plant-card">
+           <img className="plant-image" src={plant.img} alt={plant.name} />
+           <div className="plant-details">
+             <h2 className="plant-name">{plant.name}</h2>
+             <p className="plant-scientific-name">{plant.scientificName}</p>
+             <p>{plant.description}</p>
+             <div className="plant-bottom-row">
+               <p>${plant.price}</p>
+               <p>☀️ {plant.light}</p>
+               <p>💦 {plant.watering}x/month</p>
+             </div>
+             <button
+               className="plant-button"
+               onClick={() => this.props.addToCart(plant)}
+             >
+               Add to cart
+             </button>
+           </div>
+         </div>
+       ))}
+   */
